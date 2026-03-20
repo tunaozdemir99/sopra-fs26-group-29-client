7049baf1d4449906fc0f364cd8f80928ef671c8a
tunaozdemir99 <tunaozdemir99@gmail.com>
2026-03-20 13:08:02 +0100

    fix: Group Name text color

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import styles from "@/styles/page.module.css";
 
 export default function Home() {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <h1>Group 29</h1>
+      <h1 style={{ color: "red" }}>Group 29</h1>
     </div>
   );
 }
